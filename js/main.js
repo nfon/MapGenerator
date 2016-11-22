fa2f4ec57806f614a1c1a61bc89c8bc69b0ba3aa
nfon <nfon@users.noreply.github.com>
2016-11-22 14:26:39 +0100

    Add of the rivers, the size of the map and some bugfixes

--- js/main.js
+++ js/main.js
@@ -1,77 +1,90 @@
 var mapL = 100;
 var mapH = 100;
 var map = new Array();
+var debugMode = false;
 
+function clearMessage() {
+	$("#messages").html("");
+}
 
-function getRandom(min, max){
+function displayMessage(msg) {
+	if (debugMode)
+		$("#messages").append(msg+"</br>");
+	else
+		console.log(msg);
+}
+
+function getRandom(min, max) {
   	return Math.floor(Math.random() * (max - min +1)) + min;
 }
 
-function getRandomNormal(min, max){
+function getRandomNormal(min, max) {
 	return Math.min(max,Math.max(min,Math.floor(chance.normal({mean: ((min+max)/2), dev: ((min+max)/4)}))));
 }
 
-function createMap(heightMin, heightMax, summitNb, sourceNb){
-	console.log("Map generation");
+function createMap(heightMin, heightMax, summitNb, lakeNb, riverNb) {
+	clearMessage();
+	displayMessage("Map generation");
 	generateMap(heightMin,Math.floor((heightMin+heightMax)/2));
-	console.log("Creation of the summits");
+	displayMessage("Creation of the summits");
 	generateSummits(summitNb,heightMax);
-	console.log("Creation of the moutains");
+	displayMessage("Creation of the moutains");
 	generateMountains(heightMin,heightMax);
-	console.log("Creation of the sources");
-	generateSources(sourceNb,heightMin);
-	console.log("Creation of the rivers");
-	generateRivers(heightMin,heightMax);
-	generateRivers(heightMin,heightMax);//second pass
+	displayMessage("Creation of the sources");
+	generateLakesSource(lakeNb,heightMin);
+	displayMessage("Creation of the lakes");
+	generateLakes(heightMin,heightMax);
+	displayMessage("Creation of the rivers");
+	generateRivers(riverNb,heightMin);
 }
 
-function generateMap(heightMin, heightMax){
-	for (var i=0;i<mapH;i++){
+function generateMap(heightMin, heightMax) {
+	for (var i=0;i<mapH;i++) {
 		map[i] = new Array();
-		for (var j=0;j<mapL;j++){
+		for (var j=0;j<mapL;j++) {
 			map[i][j]=getRandomNormal(heightMin,heightMax);
 		}
 	}
 }
 
-function generateSummits(summitNb,heightMax){
-	for (var i=0;i<summitNb;i++){
-		var x = getRandom(0,mapL-1);
-		var y = getRandom(0,mapH-1);
+function generateSummits(summitNb,heightMax) {
+	for (var i=0;i<summitNb;i++) {
+		var x = getRandom(0,mapH-1);
+		var y = getRandom(0,mapL-1);
 		map[x][y]=heightMax;
 		changeSurroundings(x,y,heightMax);
 	}
 }
 
-function generateMountains(heightMin,heightMax){
-	for (var i=heightMax; i>heightMin+1; i--){
+function generateMountains(heightMin,heightMax) {
+	for (var i=heightMax; i>heightMin+1; i--) {
 		generateRelief(i);
 	}
 }
 
-function generateRelief(val){
-	console.log("Generate level "+val);
-	for (var i=0;i<mapH;i++){
-		for (var j=0;j<mapL;j++){
-			if (map[i][j]==val){
+function generateRelief(val) {
+	displayMessage("Generate level "+val);
+	for (var i=0;i<mapH;i++) {
+		for (var j=0;j<mapL;j++) {
+			if (map[i][j]==val) {
 				changeSurroundings(i,j,val-1);
 			}
 		}
 	}
 }
 
-function changeSurroundings(y,x,val){
+function changeSurroundings(y,x,val) {
 	var mountainSizeXMin=getRandomNormal(0,5);
 	var mountainSizeXMax=getRandomNormal(0,5);
 	var mountainSizeYMin=getRandomNormal(0,5);
 	var mountainSizeYMax=getRandomNormal(0,5);
 
-	for (var j=(x-mountainSizeXMin);j<=(x+mountainSizeXMax);j++){
-		for (var i=(y-mountainSizeYMin);i<=(y+mountainSizeYMax);i++){
-			if (i>=0 && i<mapH){
-				if (j>=0 && j<mapL){
-					if (x!=j || y!=i){
-						if (map[i][j]<val){
+	for (var j=(x-mountainSizeXMin);j<=(x+mountainSizeXMax);j++) {
+		for (var i=(y-mountainSizeYMin);i<=(y+mountainSizeYMax);i++) {
+			if (i>=0 && i<mapH) {
+				if (j>=0 && j<mapL) {
+					if (x!=j || y!=i) {
+						if (map[i][j]<val) {
 							map[i][j]=getRandomNormal(val+1,val-1);
 						}
 					}
@@ -81,43 +94,42 @@ function changeSurroundings(y,x,val){
 	}
 }
 
-function generateSources(sourceNb,heightMin){
-	for (var i=0;i<sourceNb;i++) {
-		var x = getRandom(0,mapL-1);
-		var y = getRandom(0,mapH-1);
+function generateLakesSource(lakeNb,heightMin) {
+	for (var i=0;i<lakeNb;i++) {
+		var x = getRandom(0,mapH-1);
+		var y = getRandom(0,mapL-1);
 		map[x][y]=-map[x][y];
 	}
 }
 
-function generateRivers(heightMin,heightMax){
-	for (var i=-heightMax; i<-(heightMin+1); i++){
-		generateRiver(i);
+function generateLakes(heightMin,heightMax) {
+	for (var i=-heightMax; i<-(heightMin+1); i++) {
+		generateLake(i);
 	}
 }
 
-function generateRiver(val){
-	console.log("Generate River level "+val);
+function generateLake(val) {
+	displayMessage("Generate River level "+val);
 	for (var i=0;i<mapH;i++){
 		for (var j=0;j<mapL;j++){
 			if (map[i][j]==val){
-				changeSurroundingsRiver(i,j,Math.abs(val-1));
+				changeSurroundingsLake(i,j,Math.abs(val-1));
 			}
 		}
 	}
 }
 
-
-function changeSurroundingsRiver(y,x,val){
-	var riverSizeXMin=getRandomNormal(0,3);
+function changeSurroundingsLake(y,x,val) {
+	var riverSizeXMin=getRandomNormal(0,4);
 	var riverSizeXMax=getRandomNormal(0,3);
-	var riverSizeYMin=getRandomNormal(0,3);
+	var riverSizeYMin=getRandomNormal(0,4);
 	var riverSizeYMax=getRandomNormal(0,3);
 
-	for (var j=(x-riverSizeXMin);j<=(x+riverSizeXMax);j++){
-		for (var i=(y-riverSizeYMin);i<=(y+riverSizeYMax);i++){
-			if (i>=0 && i<mapH){
-				if (j>=0 && j<mapL){
-					if (x!=j || y!=i){
+	for (var j=(x-riverSizeXMin);j<=(x+riverSizeXMax);j++) {
+		for (var i=(y-riverSizeYMin);i<=(y+riverSizeYMax);i++) {
+			if (i>=0 && i<mapH) {
+				if (j>=0 && j<mapL) {
+					if (x!=j || y!=i) {
 						if (map[i][j]<val && map[i][j]>=0) {
 							map[i][j]=-map[i][j];
 						}
@@ -128,12 +140,112 @@ function changeSurroundingsRiver(y,x,val){
 	}
 }
 
+function generateRivers(riverNb,heightMin) {
+	for (var i=0;i<riverNb;i++) {
+		var side = getRandom(0,1);
+		var orientation;
+		if (side) {
+			if (getRandom(0,1)) {
+				var x = 0;
+				orientation=0;
+			}
+			else {
+				var x = mapH-1;
+				orientation=2;
+			}
 
-function loadMap(){
+			var y = getRandom(0,mapL-1);
+		}
+		else {
+			if (getRandom(0,1)) {
+				var y = 0;
+				orientation=3;
+			}
+			else {
+				var y = mapL-1;	
+				orientation=1;			
+			}
+			var x = getRandom(0,mapH-1);
+		}
+
+		map[x][y]=-map[x][y];
+		drawRiver(x,y,orientation);
+	}
+}
+
+/*
+0 1 2
+3 x 4
+5 6 7
+*/
+function drawRiver(x,y,orientation) {
+	var curHeight = -map[x][y];
+	var inside=true;
+	var i=prevI=x;
+	var j=prevJ=y;
+	var length = 0;
+	var direction = 0;
+	var safety=0;
+	while (inside) {
+		if (length>(mapH*mapL)/2)
+			var direction = getRandom(0,8);
+		else
+		{
+			switch(orientation) {
+				case 0 : direction = getRandom(0,4)+3; break;
+				case 1 : direction = chance.pickone([0,1,3,5,6]); break;
+				case 2 : direction = getRandom(0,4);  break;
+				case 3 : direction = chance.pickone([1,2,4,6,7]);  break;
+			}
+		}
+		switch(direction) {
+			case 0 : i=i-1; j=j-1; break;
+			case 1 : i=i-1; break;
+			case 2 : i=i-1; j=j+1; break;
+			case 3 : j=j-1; break;
+			case 4 : j=j+1; break;
+			case 5 : i=i+1; j=j-1; break;
+			case 6 : i=i+1; break;
+			case 7 : i=i+1; j=j+1; break;
+		}
+
+
+		i = Math.min(mapH-1,Math.max(0,i));
+		j = Math.min(mapH-1,Math.max(0,j));
+		
+
+		if ( ( /*map[i][j]<0 ||*/ i==0 || i==mapH-1 || j==0 || j==mapL-1 ) )
+		{
+			inside=false;
+		}
+		if( map[i][j] > 0 ) {
+			length++;
+			if (map[i][j]<=curHeight) {
+				safety=0;
+				curHeight = map[i][j]
+				map[i][j] = -map[i][j];
+				prevI=i;
+				prevJ=j;
+			}
+			else
+			{
+				if (safety<10) {
+					i=prevI;
+					j=prevJ;
+					if (safety==5)
+						curHeight++;
+					safety++;
+				}
+			}
+		}
+	}
+}
+
+function loadMap() {
 	var str = "";
-	for (var i=0;i<mapH;i++){
+	for (var i=0;i<mapH;i++) {
 		str+="<div class='line'>";
-		for (var j=0;j<mapL;j++){
+		for (var j=0;j<mapL;j++) {
 			str+="<span class='case height"+Math.abs(map[i][j])+(map[i][j]<0?' blue':'')+"'></span>";
 		}
 		str+="</div>";
@@ -142,8 +254,11 @@ function loadMap(){
 }
 
 
-$(document).ready(function(){
+$(document).ready(function() {
 	$("#mapSettings").on("submit",function(evt){
+		mapH = parseInt($("input[name=mapHeight]").val(),10);
+		mapL = parseInt($("input[name=mapWidth]").val(),10);
+
 		var heightMin = parseInt($("input[name=heightMin]").val(),10);
 		var heightMax = parseInt($("input[name=heightMax]").val(),10);
 
@@ -155,9 +270,12 @@ $(document).ready(function(){
 		}
 
 		var summitNb = parseInt($("input[name=summitNb]").val(),10);
-		var sourceNb = parseInt($("input[name=sourceNb]").val(),10);
+		var lakeNb = parseInt($("input[name=lakeNb]").val(),10);
+		var riverNb = parseInt($("input[name=riverNb]").val(),10);
 
-		createMap(heightMin, heightMax, summitNb, sourceNb);
+		debugMode = $("input[name=debugMode]").prop("checked");
+
+		createMap(heightMin, heightMax, summitNb, lakeNb, riverNb);
 		loadMap();
 		return false;
 	});
