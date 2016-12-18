a2dec88607cec1f701e2c9053ba64ccf3de2e453
nfon <nfon@users.noreply.github.com>
2016-12-18 15:22:40 +0100

    bootstrap + highcharts

--- js/main.js
+++ js/main.js
@@ -1345,6 +1345,72 @@ var Opponent = function (id,name,coordinates) {
     }
 }
 
+function drawStats() {
+	Highcharts.chart('container', {
+
+        chart: {
+            polar: true,
+            type: 'line'
+        },
+
+        exporting: {
+		    buttons: {
+		        contextButton: {
+		            enabled: false
+		        }    
+		    }
+		},
+
+		credits: {
+			enabled: false
+		},
+
+        title: {
+            text: 'Team Specs',
+            x: -80
+        },
+
+        pane: {
+            size: '80%'
+        },
+
+        xAxis: {
+            categories: ['Health', 'Attack', 'Vision', 'Accuracy', 'Water', 'Food'],
+            tickmarkPlacement: 'on',
+            lineWidth: 0
+        },
+
+        yAxis: {
+            gridLineInterpolation: 'polygon',
+            lineWidth: 0,
+            min: 0
+        },
+
+        tooltip: {
+            shared: true,
+            pointFormat: '<span style="color:{series.color}">{series.name}: <b>${point.y:,.0f}</b><br/>'
+        },
+
+        legend: {
+            align: 'right',
+            verticalAlign: 'top',
+            y: 70,
+            layout: 'vertical'
+        },
+
+        series: [{
+            name: 'Jack',
+            data: [120, 110, 100, 90, 100, 100],
+            pointPlacement: 'on'
+        }, {
+            name: 'Lea',
+            data: [130, 100, 90, 100, 100, 100],
+            pointPlacement: 'on'
+        }]
+
+    });
+}
+
 $(document).ready(function() {
 	$("#lava").on("click",function(){
 		map.releaseLava();
@@ -1435,4 +1501,6 @@ $(document).ready(function() {
 		$("#speedControl").removeClass("hide");
 		return false;
 	});
+
+	drawStats();
 });
